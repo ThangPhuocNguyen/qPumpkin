6d3b989add514f41729b5c87bd9dbd55c44838aa
Thang Nguyen <thangphuocnguyen.se@gmail.com>
2015-03-30 16:56:50 +0700

    New demo with mentio

--- hero/app/scripts/app.js
+++ hero/app/scripts/app.js
@@ -14,6 +14,7 @@
       'ui.router',
       'Home',
       'Profile',
+      'mentio',
     ])
     .run([
         '$state',
--- hero/app/scripts/profile/profile-ctrl.js
+++ hero/app/scripts/profile/profile-ctrl.js
@@ -6,6 +6,49 @@
       // $scope.abc = 'profile';
       // console.log('Thang:', $scope.abc);
       console.log('Profile');
+
+      var initPeople = [
+        { label: 'Thang Nguyen'},
+        { label: 'Qtysth'},
+        { label: 'Green Turtle'},
+        { label: 'Nothing'},
+        { label: 'Something'},
+        { label: 'Everything'}
+      ]
+
+      // $scope.people = [
+      //   { label: 'Joe'},
+      //   { label: 'Mike'},
+      //   { label: 'Diane'}
+      // ]
+
+      $scope.searchPeople= function(term) {
+        console.log('here:', term);
+
+        var peopleList = [];
+
+        if ( term === '' ) {
+          $scope.people = [];
+          return;
+        }
+
+        angular.forEach( initPeople, function (item) {
+
+          if (item.label.toUpperCase().indexOf(term.toUpperCase()) >= 0) {
+
+            console.log('search:', item);
+            peopleList.push(item);
+          }
+
+        });
+
+        $scope.people = peopleList;
+      };
+
+
+      $scope.getPeopleTextRaw = function(item) {
+        return 'hehe:'+item.label;
+      };
     });
 
 })(angular);
